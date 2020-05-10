29ea5936b8453129a53c27c12c0a30c043a5afd6
ubed952 <64809637+ubed952@users.noreply.github.com>
2020-05-10 15:59:49 +0700

    Add files via upload

---
+++ drx.py
@@ -0,0 +1,7 @@
+import sys, time
+def jalan (kata):
+	for e in kata:
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.1)
+jalan('Nyuk gua mau jujur sama lu. Sebenernya gua suka sama lu udh lama. Tapi gimana mau ungkapin kalo lu itu sebenernya suka sama fadli. Gua sering ledekin lu juga karena pengen chat panjang sama lu. Nyuk Gue sayang Sama Lu. Gpp Lu Nolak Gue Yg Penting Gue Udh ungkapin Perasaan Gue sbelum Gue kehilangan Lu Karena Lulus. Love You😙'.center(44))
